be6e1228deea90b97e784977225d8a141ccda3b0
Ryazan8 <88ryazanov88@gmail.com>
2024-10-20 19:14:30 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="app">
+      <form @submit.prevent>
+        <h4>Создание поста:</h4>
+        <input
+            v-bind:value="title"
+            @input="title = $event.target.value"
+            class="input"
+            type="text"
+            placeholder="Название"
+        >
+        <input
+            v-bind:value="body"
+            @input="body = $event.target.value"
+            class="input"
+            type="text"
+            placeholder="Описание"
+        >
+        <button
+            class="btn"
+            @click="createPost"
+        >
+          Создать
+        </button>
+      </form>
+    <div class="post" v-for="post in posts">
+      <div><strong>Название:</strong>{{ post.title }}</div>
+      <div><strong>Описание:</strong>{{ post.body }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        posts: [
+          { id: 1, title: 'JavaScript', body: 'писание поста' },
+          { id: 2, title: 'JavaScript 2', body: 'писание поста 2' },
+          { id: 3, title: 'JavaScript 3', body: 'писание поста 3' },
+        ],
+        title: '',
+        body: ''
+      }
+    },
+    methods: {
+      createPost() {
+        const newPost = {
+          id: Date.now(),
+          title: this.title,
+          body: this.body,
+        }
+        this.posts.push(newPost)
+        this.title = ''
+        this.body = ''
+      },
+    }
+  }
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+
+.post{
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+
+form{
+  display: flex;
+  flex-direction: column;
+}
+
+.input{
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+
+.btn{
+  align-self: flex-end;
+  margin-top: 15px;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+  cursor: pointer;
+}
+
+</style>
