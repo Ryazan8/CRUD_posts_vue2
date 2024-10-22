481c83eaf7ec3419a3cd839931f8f0c5557d3869
Ryazan8 <88ryazanov88@gmail.com>
2024-10-22 14:19:21 +0300

    add modal "Create post"

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="app">
-    <post-form
-        @create="createPost"
-    />
+    <h1>
+      Страница с постами
+    </h1>
+    <my-button
+      @click="showDialog"
+      style="margin: 15px 0;"
+    >
+      Создать пост
+    </my-button>
+    <my-dialog v-model:show="dialogVisible">
+      <post-form
+          @create="createPost"
+      />
+    </my-dialog>
     <post-list
         :posts="posts"
         @remove="removePost"
@@ -13,9 +24,13 @@
 <script>
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
+import MyDialog from "@/components/Ui/MyDialog.vue";
+import MyButton from "@/components/Ui/MyButton.vue";
 
   export default {
     components: {
+      MyButton,
+      MyDialog,
       PostList,
       PostForm
     },
@@ -26,14 +41,19 @@ import PostList from "./components/PostList";
           { id: 2, title: 'JavaScript 2', body: 'Описание поста 2' },
           { id: 3, title: 'JavaScript 3', body: 'Описание поста 3' },
         ],
+        dialogVisible: false
       }
     },
     methods: {
       createPost(post) {
         this.posts.push(post)
+        this.dialogVisible = false
       },
       removePost(post) {
         this.posts = this.posts.filter(p => p.id !== post.id)
+      },
+      showDialog() {
+        this.dialogVisible = true
       }
     }
   }
